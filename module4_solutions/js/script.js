29780545f8d499681b48ee86c0cd0574b52cc299
sivani99343 <65996688+sivani99343@users.noreply.github.com>
2020-12-09 21:30:38 +0530

    Add files via upload

--- module4_solutions/js/script.js
+++ module4_solutions/js/script.js
@@ -1,1 +1,16 @@
+(function () {
 
+    var names = ["Sivani", "Jack", "Jenifer", "Jason", "Aroan", "Rajesh", "Sumith", "Parker", "Lason", "Jack"];
+    
+    for (var s = 0; s < names.length; s++) {
+    
+      var firstLetter = names[s].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[s]);
+      } 
+      else {
+        helloSpeaker.speak(names[s]);
+      }
+    }
+    
+    })();
